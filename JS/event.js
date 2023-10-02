05f437885ac6759df5f0790ffdd0f291089c57d6
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-10-02 13:01:21 +0600

    ADD FILES

---
+++ JS/event.js
@@ -0,0 +1,21 @@
+document.getElementById("click_button").addEventListener("click", function () {
+  const clickButton = document.getElementById("box");
+  clickButton.style.backgroundColor = "";
+  clickButton.classList.add("box_two");
+});
+document.getElementById("update_button").addEventListener("click", function () {
+  const input = document.getElementById("input");
+  const inputText = input.value;
+  const p = document.getElementById("update_text");
+  p.innerText = input.value;
+  input.value = " ";
+});
+document.getElementById("post_button").addEventListener("click", function () {
+  const inputArea = document.getElementById("comment_area");
+  const commentText = inputArea.value;
+  const commentContainer = document.getElementById("comment_container");
+  const p = document.createElement("p");
+  p.innerText = inputArea.value;
+  commentContainer.appendChild(p);
+  inputArea.value = " ";
+});
